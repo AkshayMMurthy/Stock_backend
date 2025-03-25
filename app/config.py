bc709c5d87b38bd2cf5c51c63f61a9e610000dad
AkshayMMurthy <138966239+AkshayMMurthy@users.noreply.github.com>
2025-03-26 01:37:19 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")  # ✅ Directly fetches from Render's environment variables
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY") 
 
 if GOOGLE_API_KEY is None:
     raise ValueError("GOOGLE_API_KEY is not set in environment variables")
